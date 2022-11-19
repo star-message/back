3f6355cf4fdce75f463594d5965002fcd1a95580
jeangho293 <jeangho293@naver.com>
2022-11-19 12:32:53 +0900

    [Feat]: 서버구동

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import * as express from 'express';
+
+class App {
+  private app;
+
+  constructor() {
+    this.app = express();
+  }
+
+  listen(port: string | number) {
+    this.app.listen(port, () => {
+      console.log(`http://localhost:${port}`);
+    })
+  }
+}
+
+export default App;
---
+++ src/server.ts
@@ -0,0 +1,5 @@
+import App from './app';
+
+const http = new App();
+
+http.listen(3000);
